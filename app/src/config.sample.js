aeb73b7bf181a42866d24be98e425b50755996c8
jkahn <jkahn@amazon.com>
2017-05-03 14:17:38 -0700

    Remove config.js and replace with sample

---
+++ app/src/config.sample.js
@@ -0,0 +1,16 @@
+/*
+  Configuration for my-pet-shop
+ */
+
+export default {
+  // APIs
+  petApiEndpoint: 'https://<< API_ID >>.execute-api.<< REGION >>.amazonaws.com/<< STAGE >>',
+  petApiHost: '<< API_ID >>.execute-api.<< REGION >>.amazonaws.com',
+  petApiRegion: '<< REGION >>',
+
+  // Cognito
+  cognitoUserPoolId: '<< COGNITO_USER_POOL_ID >>',
+  cognitoUserPoolAppClientId: '<< COGNITO_USER_POOL_APP_ID >>',
+  cognitoIdentityPoolId: '<< COGNITO_IDENTITY_POOL_ID >>',
+  cognitoUserPoolRegion: '<< REGION >>'
+}
