d74bdcf6032143232eaef0ba1d9ff5fd6c39c056
jkahn <jkahn@amazon.com>
2017-05-03 20:40:42 -0700

    Add pet api service with AWS SigV4 signing

---
+++ app/src/api/pets.js
@@ -0,0 +1,51 @@
+/**
+ *
+ */
+
+import axios from 'axios'
+
+import SignedRequest from './signed-request'
+
+export default {
+  async getPets () {
+    let signedRequest = SignedRequest.build('GET', '/Prod/pets', null)
+    let response = await axios(signedRequest)
+
+    if (response.data.success) {
+      return response.data.results
+    } else {
+      console.error('pets#list - Bad data')
+      // TODO: add real 'throw ERROR'
+    }
+  },
+
+  async getPetById (id) {
+    let signedRequest = SignedRequest.build('GET', `/Prod/pets/${id}`, null)
+    let response = await axios(signedRequest)
+
+    if (response.data.success) {
+      return response.data.result
+    } else {
+      console.error('pets#get - Bad data')
+      // TODO: add real 'throw ERROR'
+    }
+  },
+
+  /**
+   * Update the pet record on the server.
+   *
+   * @param  {[JSON} pet form data describing updates to pet
+   */
+  async updatePet (pet) {
+    let signedRequest = SignedRequest.build('PUT', `/Prod/pets/${pet.uuid}`, pet)
+    let response = await axios(signedRequest)
+
+    // let response = await http.put(`pets/${pet.uuid}`, pet)
+    if (response.status === 200) {
+      return Promise.resolve()
+    } else {
+      console.error('pets#update - Bad data')
+      return Promise.reject()
+    }
+  }
+}
---
+++ app/src/api/signed-request.js
@@ -0,0 +1,45 @@
+/**
+ * SignedRequest class builds requests signed with AWS SigV4.
+ */
+
+import AWS from 'aws-sdk'
+import aws4 from 'aws4'
+
+import Config from '../config'
+
+const build = (method, path, data) => {
+  console.log(AWS.config.credentials)
+
+  path = path.startsWith('/') ? path : `/${path}`
+
+  let request = {
+    host: Config.petApiHost,
+    method: method,
+    url: `https://${Config.petApiHost}${path}`,
+    path: path
+  }
+
+  if (method === 'PUT' || method === 'POST') {
+    request['body'] = JSON.stringify(data)
+    request['data'] = data
+    request['headers'] = {
+      'content-type': 'application/json'
+    }
+  }
+
+  let signedRequest = aws4.sign(request,
+    {
+      secretAccessKey: AWS.config.credentials.secretAccessKey,
+      accessKeyId: AWS.config.credentials.accessKeyId,
+      sessionToken: AWS.config.credentials.sessionToken
+    })
+
+  delete signedRequest.headers['Host']
+  delete signedRequest.headers['Content-Length']
+
+  return signedRequest
+}
+
+export default {
+  build
+}
